75edf6bc23b1e54c3c97d911ab64b9c48731da76
shgtkshruch <shgtk.shruch@gmail.com>
2019-12-30 22:08:20 +0900

    Add header component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,10 @@
 import React from "react"
+import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `teal` }}>
-    <h1>About Gatsby</h1>
+    <Header headerText="About Gatsby" />
+    <Header headerText="It's pretty cool" />
     <p>Such wow. Very React.</p>
   </div>
 )
